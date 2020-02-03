f93bc7e8c112772ad23c458c007c084170cb290b
‘Yonatan <yonatan@yaltman.com>
2020-02-03 23:23:29 +0200

    add all files

---
+++ MyLastVbMvc/Controllers/HomeController.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class HomeController
+        Inherits Controller
+
+        ' GET: Home
+        Function Index() As ActionResult
+            ' ViewBag
+            ViewBag.Message = "Message from ViewBag!"
+
+            ' ViewData
+            ViewData("School") = "Academka"
+
+
+            Return View()  'same like => Return View("Index")
+
+        End Function
+    End Class
+End Namespace
